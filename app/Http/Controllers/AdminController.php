bd5dac297a95a2f1a4abf422faa4f62333b0444e
sanadajv1 <sanadajv@student.apc.edu.ph>
2024-01-15 01:37:28 +0800

    delete and update product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -57,6 +57,52 @@ class AdminController extends Controller
        
 
     }
-    
+    public function show_product(){
+        $product=product::all();
+      return view ('admin.show_product', compact('product'));
+    }
+
+    public function delete_product($id){
+        $product=product::find($id);
+        $product->delete();
+
+        return redirect()->back()->with('message', 'Product Deleted Successfully');
+    }
+
+    public function update_product($id){
+        $product=product::find($id);
+
+        $category=category::all();
+      return view ('admin.update_product', compact('product','category'));
+    }
+
+    public function  update_product_confirm(Request $request, $id){
+        $product=product::find($id);
+
+
+        $product->product_name=$request->product_name;
+        $product->category=$request->category;
+        $product->product_description=$request->product_description;
+        $product->price=$request->price;
+        $product->processing_time=$request->processing_time;
+
+        $image = $request->image;
+        if($image){
+            $imagename=time().'.'.$image->getClientOriginalExtension();
+
+            $request->image->move('product',$imagename);
+            $product->image=$imagename;
+        }
+
+        
+
+        
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Updated Successfully');
+    }
+
+
 
 }
