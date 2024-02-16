c049f0b6578c234e1197c17eda68b415bb8fd673
JamesTabulao <tabulaojl@student.apc.edu.ph>
2024-02-16 18:36:06 +0800

    On Process

--- resources/views/admin/onprocess.blade.php
+++ resources/views/admin/onprocess.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     @include('admin.css')
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/onprocess.css') }}">
+    <link rel="stylesheet" href="admin/assets/css/admin_onprocess.css">
     <link rel="stylesheet" href="admin/assets/css/ordertracking.css">
 </head>
 <body>
@@ -15,46 +15,55 @@
     @include('admin.header')
     <!-- partial -->
     <div class="main-panel">
-        <div class="content-wrapper">
-            <h1 class="title-deg">Order Processing</h1>
-
-
-
+        <div class="">
             <section class="custom-section">
-                <div class="container py-5">
+                <div class="">
                     <div class="order-container">
                         <!-- Loop through your order data here -->
                         @foreach ($order as $order)
                             @if ($order->order_status === 'On Process' || $order->order_status === 'To Pay')
                                 <div class="order-item">
-                                    <div>
-                                        <p class="product-name">{{ $order->name }}</p>
-                                    </div>
-                                    <img src="{{ asset('product/' . $order->image) }}" class="img-fluid" alt="{{ $order->product_name }}">
-                                    <div class="order-details">
-                                        <p class="product-name">{{ $order->product_name }}</p>
-                                        <p class="order-info">Quantity: {{ $order->quantity }}</p>
-                                        <p class="order-info">Price: ${{ $order->price }}</p>
-                                        <p class="order-info">Order Status:  @if($order->order_status=='Order Placed')
-                                                Pending
-                                            @else
-                                                {{ $order->order_status }}
-                                            @endif
-                                        </p>
-
-                                        <!-- Add buttons inside each order -->
-                                        <div class="buttons-container">
-                                            @if ($order->order_status === 'On Process')
-                                                <a href="{{url('to_fpay', $order->id)}}" class="btn btn-success" onclick="return confirm('Are you sure this order is done?')">Done</a>
-                                                <a href="{{ url('to_ship', $order->id) }}" class="btn btn-action btn-low-opacity btn-confirm-payment disabled" onclick="confirmOrderPayment({{ $order->id }})">Confirm Payment</a>
-                                            @endif
-
-                                            @if ($order->order_status === 'To Pay')
-                                            <a href="{{url('to_fpay', $order->id)}}" class="btn btn-action btn-low-opacity disabled" onclick="return confirm('Are you sure this order can be made?')">Done</a>
-                                            <a href="{{url('to_ship', $order->id)}}" class="btn btn-success" onclick="return confirm('Are you sure this order can be made?')">Confirm Payment</a>
-                                            @endif
+                                        <div class="column-1">
+                                            <p class="customer-name">{{ $order->name }}</p>
+                                            <p class="customer-num">1</p>
                                         </div>
-                                    </div>
+                                        
+                                        
+                                        
+                                        <div class="column-2">
+                                            <div>
+                                                <img src="{{ asset('product/' . $order->image) }}" class="img-fluid" alt="{{ $order->product_name }}">
+                                            </div>
+                                            <div class="product-details">
+                                                <p class="product-name">{{ $order->product_name }}</p>
+                                                <p class="order-info">Quantity: {{ $order->quantity }}</p>
+                                                
+                                                <p class="order-info">Order Status:  @if($order->order_status=='Order Placed')
+                                                        Pending
+                                                    @else
+                                                        {{ $order->order_status }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div class="column-3">
+                                            <p class="order-info">Order Total: <b>₱{{ $order->price }}</b></p>
+                                        </div>
+
+                                            <!-- Add buttons inside each order -->
+                                            <div class="column-4">
+                                                @if ($order->order_status === 'On Process')
+                                                    <a href="{{url('to_fpay', $order->id)}}" class="btn btn-success" onclick="return confirm('Are you sure this order is done?')">Done</a>
+                                                    <a href="{{ url('to_ship', $order->id) }}" class="btn btn-action btn-low-opacity btn-confirm-payment disabled" onclick="confirmOrderPayment({{ $order->id }})">Paid</a>
+                                                @endif
+
+                                                @if ($order->order_status === 'To Pay')
+                                                <a href="{{url('to_fpay', $order->id)}}" class="btn btn-action btn-low-opacity disabled" onclick="return confirm('Are you sure this order can be made?')">Done</a>
+                                                <a href="{{url('to_ship', $order->id)}}" class="btn btn-success" onclick="return confirm('Are you sure this order can be made?')">Paid</a>
+                                                @endif
+                                            </div>
+                                    
                                 </div>
                             @endif
                         @endforeach
