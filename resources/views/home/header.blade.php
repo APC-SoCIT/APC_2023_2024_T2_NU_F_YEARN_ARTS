77a64d4c81a234028924d22921f3ef146c29905e
sanadajv1 <sanadajv@student.apc.edu.ph>
2024-03-01 09:18:22 +0800

    Update header.blade.php

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -23,9 +23,22 @@
             @if (Route::has('login'))
 
             @auth
-            <x-app-layout>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="{{ route('profile.show') }}">Profile</a>
 
-            </x-app-layout>
+                    <div class="dropdown-divider"></div>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <input type="submit" value="Logout">
+
+                    </form>
+                </div>
+            </div>
             @else
             <a href="login" class="Login">Log in</a>
             <a href="register" class="Signup">Signup</a>
