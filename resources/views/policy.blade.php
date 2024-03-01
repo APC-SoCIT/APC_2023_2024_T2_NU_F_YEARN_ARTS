726db63f12ee83bd5a726b17d2ce050861c7ca88
JamesTabulao <tabulaojl@student.apc.edu.ph>
2024-03-01 17:44:29 +0800

    Terms and policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -6,7 +6,28 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $policy !!}
+            <b>Privacy Policy:</b>
+            <br><br>
+<b>1. Information Collection</b>
+<br>
+We collect personal information that you voluntarily provide, including but not limited to, name, email address, and other contact information. We may also collect non-personal information such as IP addresses and browser information.
+<br>
+<b>2. Use of Information</b>
+<br>
+We may use your information to personalize your experience, improve our website, send periodic emails, and for other legitimate business purposes. We do not sell, trade, or otherwise transfer your personal information to third parties without your consent.
+<br>
+<b>3. Cookies</b>
+<br>
+We use cookies to enhance your experience on our site. You can choose to disable cookies through your browser settings, but this may affect your ability to use certain features of the site.
+<br>
+<b>4. Security</b>
+<br>
+We take reasonable measures to protect your personal information, but we cannot guarantee the security of your data transmitted to us. You transmit your information at your own risk.
+<br>
+<b>5. Changes to the Privacy Policy</b>
+<br>
+We reserve the right to update our Privacy Policy at any time. You should check this page periodically for changes.
+
             </div>
         </div>
     </div>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -6,7 +6,27 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $terms !!}
+            <b>Terms of Service:</b>
+<br><br>
+<b>1. Acceptance of Terms</b>
+<br>
+By accessing or using Yearn Art , you agree to be bound by these Terms of Service and all applicable laws and regulations. If you do not agree with any of these terms, you are prohibited from using or accessing this site.
+<br>
+<b>2. Use of the Service</b>
+<br>
+Yearn Art  grants you a non-exclusive, non-transferable, revocable license to use the service for personal, non-commercial use only. You may not use the service for any illegal or unauthorized purpose.
+<br>
+<b>3. User Account</b>
+<br>
+To access certain features of the service, you may be required to create an account. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your account.
+<br>
+<b>4. Content</b>
+<br>
+You retain ownership of any content you submit to the service. By submitting content, you grantYearn Art ] a worldwide, non-exclusive, royalty-free license to use, reproduce, modify, adapt, publish, translate, and distribute such content.
+<br>
+<b>5. Termination</b>
+<br>
+Yearn Art reserves the right to terminate or suspend your account and access to the service at any time, without notice, for any reason.
             </div>
         </div>
     </div>
